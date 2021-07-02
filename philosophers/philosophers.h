8f37c2b30282384a67b9c31c4765bb784ed688ab
LeoProko <leoproko126@gmail.com>
2021-07-02 22:28:19 +0300

    [philosophers] add solution of 'philosophers' problem

--- philosophers/philosophers.h
+++ philosophers/philosophers.h
@@ -4,51 +4,60 @@
 
 
 class Fork {
- public:
-  Fork(size_t id) : id_(id) {
-  }
+public:
+    Fork(size_t id)
+        : id_(id) {}
 
-  size_t Id() const {
-    return id_;
-  }
+    size_t Id() const {
+        return id_;
+    }
 
-  void Get() {
-    mutex_.lock();
-  }
+    void Get() {
+        mutex.lock();
+    }
 
-  bool TryGet() {
-    return mutex_.try_lock();
-  }
+    bool TryGet() {
+        return mutex.try_lock();
+    }
 
-  void Put() {
-    mutex_.unlock();
-  }
+    void Put() {
+        mutex.unlock();
+    }
 
- private:
-  size_t id_;
-  std::mutex mutex_;
+private:
+    size_t id_;
+    std::mutex mutex;
 };
 
 class Philosopher {
- public:
-  Philosopher(size_t /*id*/, Fork* /*left_fork*/, Fork* /*right_fork*/) {
-    // Your code
-  }
+public:
+    Philosopher(size_t id_, Fork* left_fork_, Fork* right_fork_)
+        : id(id_)
+        , left_fork(left_fork_)
+        , right_fork(right_fork_) {}
 
-  size_t Id() const {
-    // Your code
-    return -1;
-  }
+    size_t Id() const {
+        return id;
+    }
 
-  void Eat() {
-    // Your code
-  }
+    void Eat() {
+        if (id == 1) {
+            while (!right_fork->TryGet());
+            while (!left_fork->TryGet());
+            return;
+        }
+        while (!left_fork->TryGet());
+        while (!right_fork->TryGet());
+    }
 
-  void Think() {
-    // Your code
-  }
+    void Think() {
+        left_fork->Put();
+        right_fork->Put();
+    }
 
- private:
-  // Your code
+private:
+    size_t id;
+    Fork* left_fork;
+    Fork* right_fork;
 };
 
