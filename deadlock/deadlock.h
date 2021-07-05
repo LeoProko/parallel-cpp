f0b09aa7f085a9a59864b3a631b22d391e59d6b4
LeoProko <leoproko126@gmail.com>
2021-07-05 21:32:31 +0300

    [deadlock] variables renaming

--- deadlock/deadlock.h
+++ deadlock/deadlock.h
@@ -6,22 +6,22 @@
 
 class Deadlock {
 private:
-    std::mutex m_1;
-    std::mutex m_2;
+    std::mutex mutex_1;
+    std::mutex mutex_2;
 public:
     Deadlock() {
     }
 
     void ThreadOne() {
-        std::unique_lock<std::mutex> lock_1(m_1);
+        std::unique_lock<std::mutex> lock_1(mutex_1);
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
-        std::unique_lock<std::mutex> lock_2(m_2);
+        std::unique_lock<std::mutex> lock_2(mutex_2);
     }
 
     void ThreadTwo() {
-        std::unique_lock<std::mutex> lock_2(m_2);
+        std::unique_lock<std::mutex> lock_2(mutex_2);
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
-        std::unique_lock<std::mutex> lock_1(m_1);
+        std::unique_lock<std::mutex> lock_1(mutex_1);
     }
 };
 
