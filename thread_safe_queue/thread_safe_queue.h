470be011ac1ebaa9663e1b66dd2d21255a98aef7
LeoProko <leoproko126@gmail.com>
2021-07-06 09:39:32 +0300

    [thread_safe_queue] add solution of 'thread_safe_queue' problem

--- thread_safe_queue/thread_safe_queue.h
+++ thread_safe_queue/thread_safe_queue.h
@@ -2,39 +2,47 @@
 
 #include <optional>
 #include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <thread>
 
 
 template <typename T>
 class ThreadSafeQueue {
- public:
-  ThreadSafeQueue() {
-  }
-
-  void Push(const T& value) {
+private:
     // Your code
-    queue_.push(value);
-  }
+    std::queue<T> queue;
+    std::mutex mutex;
+    std::condition_variable condition_variable;
 
-  T Pop() {
-    // Your code
-    auto value = queue_.front();
-    queue_.pop();
-    return value;
-  }
-
-  std::optional<T> TryPop() {
-    // Your code
-    if (queue_.empty()) {
-      return std::nullopt;
+public:
+    ThreadSafeQueue() {
     }
-    auto value = queue_.front();
-    queue_.pop();
-    return value;
-  }
 
+    void Push(const T& value) {
+        std::unique_lock<std::mutex> lock(mutex);
+        queue.push(value);
+        condition_variable.notify_one();
+    }
 
- private:
-  // Your code
-  std::queue<T> queue_;
+    T Pop() {
+        std::unique_lock<std::mutex> lock(mutex);
+        while (queue.empty()) {
+            condition_variable.wait(lock);
+        }
+        auto value = queue.front();
+        queue.pop();
+        return value;
+    }
+
+    std::optional<T> TryPop() {
+        std::unique_lock<std::mutex> lock(mutex);
+        if (queue.empty()) {
+          return std::nullopt;
+        }
+        auto value = queue.front();
+        queue.pop();
+        return value;
+    }
 };
 
