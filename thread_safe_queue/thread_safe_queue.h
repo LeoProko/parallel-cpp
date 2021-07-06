31f3a06aa0719ac52534df75e3b9847cc0be2fb6
LeoProko <leoproko126@gmail.com>
2021-07-06 13:48:18 +0300

    [thread_safe_queue] remove comments

--- thread_safe_queue/thread_safe_queue.h
+++ thread_safe_queue/thread_safe_queue.h
@@ -10,10 +10,9 @@
 template <typename T>
 class ThreadSafeQueue {
 private:
-    // Your code
     std::queue<T> queue_;
     std::mutex queue_mutex_;
-    std::condition_variable condition_variable;
+    std::condition_variable condition_variable_;
 
 public:
     ThreadSafeQueue() = default;
@@ -21,13 +20,13 @@ public:
     void Push(const T& value) {
         std::unique_lock<std::mutex> lock(queue_mutex_);
         queue_.push(value);
-        condition_variable.notify_one();
+        condition_variable_.notify_one();
     }
 
     T Pop() {
         std::unique_lock<std::mutex> lock(queue_mutex_);
         while (queue_.empty()) {
-            condition_variable.wait(lock);
+            condition_variable_.wait(lock);
         }
         auto value = queue_.front();
         queue_.pop();
