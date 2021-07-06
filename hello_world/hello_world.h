cdd254945cb0e8759d9139b8d565763797f45544
LeoProko <leoproko126@gmail.com>
2021-07-06 12:33:36 +0300

    [hello_world] move threads from class objects to function

--- hello_world/hello_world.h
+++ hello_world/hello_world.h
@@ -9,24 +9,23 @@
 
 class HelloWorld {
 private:
-    std::vector<std::thread> threads;
-    std::mutex mutex;
-
-    const size_t num_threads;
+    std::mutex mutex_;
+    const size_t num_threads_;
 
     void hello_world_print(std::ostream& os) {
-        std::unique_lock<std::mutex> lock(mutex);
+        std::unique_lock<std::mutex> lock(mutex_);
         os << kHelloPrefix << std::this_thread::get_id() << "\n";
     }
 
 public:
     static constexpr std::string_view kHelloPrefix = "Hello, World! From thread ";
 
-    HelloWorld(size_t n_threads)
-        : num_threads(n_threads) {}
+    HelloWorld(size_t num_threads)
+        : num_threads_(num_threads) {}
 
     void SayHello(std::ostream& os) {
-        for (size_t i = 0; i != num_threads; ++i) {
+        std::vector<std::thread> threads;
+        for (size_t i = 0; i != num_threads_; ++i) {
             threads.emplace_back([&] {
                         hello_world_print(os);
                     });
