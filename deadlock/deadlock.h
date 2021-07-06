9de0193d3c48414a385194db944af0aa32bb18a6
LeoProko <leoproko126@gmail.com>
2021-07-06 12:43:20 +0300

    [deadlocks] class objects renaming

--- deadlock/deadlock.h
+++ deadlock/deadlock.h
@@ -6,22 +6,22 @@
 
 class Deadlock {
 private:
-    std::mutex mutex_1;
-    std::mutex mutex_2;
+    std::mutex mutex_1_;
+    std::mutex mutex_2_;
 public:
     Deadlock() {
     }
 
     void ThreadOne() {
-        std::unique_lock<std::mutex> lock_1(mutex_1);
+        std::unique_lock<std::mutex> lock_1(mutex_1_);
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
-        std::unique_lock<std::mutex> lock_2(mutex_2);
+        std::unique_lock<std::mutex> lock_2(mutex_2_);
     }
 
     void ThreadTwo() {
-        std::unique_lock<std::mutex> lock_2(mutex_2);
+        std::unique_lock<std::mutex> lock_2(mutex_2_);
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
-        std::unique_lock<std::mutex> lock_1(mutex_1);
+        std::unique_lock<std::mutex> lock_1(mutex_1_);
     }
 };
 
