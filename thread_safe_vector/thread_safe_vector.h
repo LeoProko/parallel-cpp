77edf05025bf4a20f226b1d080506b315c62189b
LeoProko <leoproko126@gmail.com>
2021-07-06 13:30:04 +0300

    [thread_safe_vector] add solution of 'thread_safe_vector' problem

--- thread_safe_vector/thread_safe_vector.h
+++ thread_safe_vector/thread_safe_vector.h
@@ -9,32 +9,31 @@
 template <typename T>
 class ThreadSafeVector {
 private:
-    mutable std::shared_mutex shared_mutex;
+    mutable std::shared_mutex read_mutex;
+    mutable std::mutex write_mutex;
     std::atomic<int> vector_size = 0;
-    std::vector<T> vector;
+    std::vector<T> vector_;
     bool is_reallocating = false;
 
 public:
     ThreadSafeVector() = default;
 
     T operator[](size_t index) const {
-        std::shared_lock<std::shared_mutex> shared_lock(shared_mutex);
-        while (is_reallocating);
-        return vector[index];
+        std::shared_lock<std::shared_mutex> read_lock(read_mutex);
+        return vector_[index];
     }
 
     size_t Size() const {
-        std::shared_lock<std::shared_mutex> shared_lock(shared_mutex);
-        return vector_size;
+        return vector_.size();
     }
 
     void PushBack(const T& value) {
-        std::shared_lock<std::shared_mutex> shared_lock(shared_mutex);
-        if (vector.size() == vector.capacity()) {
-            is_reallocating = true;
+        std::unique_lock<std::mutex> write_lock(write_mutex);
+        if (vector_.size() == vector_.capacity()) {
+            std::unique_lock<std::shared_mutex> read_lock(read_mutex);
+            vector_.push_back(value);
+        } else {
+            vector_.push_back(value);
         }
-        vector.push_back(value);
-        ++vector_size;
-        is_reallocating = false;
     }
 };
