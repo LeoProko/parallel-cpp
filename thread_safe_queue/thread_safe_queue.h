be7430d454466c73800b81c3d63c84e6976b43df
LeoProko <leoproko126@gmail.com>
2021-07-06 13:38:52 +0300

    [thread_safe_queue] class objects renaming

--- thread_safe_queue/thread_safe_queue.h
+++ thread_safe_queue/thread_safe_queue.h
@@ -11,37 +11,36 @@ template <typename T>
 class ThreadSafeQueue {
 private:
     // Your code
-    std::queue<T> queue;
-    std::mutex mutex;
+    std::queue<T> queue_;
+    std::mutex queue_mutex_;
     std::condition_variable condition_variable;
 
 public:
-    ThreadSafeQueue() {
-    }
+    ThreadSafeQueue() = default;
 
     void Push(const T& value) {
-        std::unique_lock<std::mutex> lock(mutex);
-        queue.push(value);
+        std::unique_lock<std::mutex> lock(queue_mutex_);
+        queue_.push(value);
         condition_variable.notify_one();
     }
 
     T Pop() {
-        std::unique_lock<std::mutex> lock(mutex);
-        while (queue.empty()) {
+        std::unique_lock<std::mutex> lock(queue_mutex_);
+        while (queue_.empty()) {
             condition_variable.wait(lock);
         }
-        auto value = queue.front();
-        queue.pop();
+        auto value = queue_.front();
+        queue_.pop();
         return value;
     }
 
     std::optional<T> TryPop() {
-        std::unique_lock<std::mutex> lock(mutex);
-        if (queue.empty()) {
+        std::unique_lock<std::mutex> lock(queue_mutex_);
+        if (queue_.empty()) {
           return std::nullopt;
         }
-        auto value = queue.front();
-        queue.pop();
+        auto value = queue_.front();
+        queue_.pop();
         return value;
     }
 };
