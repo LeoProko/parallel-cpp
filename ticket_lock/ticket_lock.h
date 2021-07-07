041c2790a169cd1c8edfeb7bfe14be803d2ee4ec
LeoProko <leoproko126@gmail.com>
2021-07-07 10:54:45 +0300

    [ticket_lock] add solution of 'ticket_lock' problem

--- ticket_lock/ticket_lock.h
+++ ticket_lock/ticket_lock.h
@@ -1,20 +1,26 @@
 #pragma once
 
+#include <atomic>
+#include <thread>
 
 class TicketLock {
- public:
-  TicketLock() {
-  }
+private:
+    std::atomic<int> current_ticket_{0};
+    std::atomic<int> next_ticket_{0};
 
-  void Lock() {
-    // Your code
-  }
+public:
+    TicketLock() = default;
 
-  void Unlock() {
-    // Your code
-  }
+    void Lock() {
+        int my_ticket_ = next_ticket_.fetch_add(1);
+        while (current_ticket_.load() != my_ticket_) {
+            std::this_thread::yield();
+        }
+        
+    }
 
- private:
-  // Your code
+    void Unlock() {
+        current_ticket_.fetch_add(1);
+    }
 };
 
