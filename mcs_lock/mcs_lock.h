d861b29e3084cd6c8690635bb21f3ea05d730045
LeoProko <leoproko126@gmail.com>
2021-07-09 16:37:33 +0300

    [mcs_lock] draft commit

--- mcs_lock/mcs_lock.h
+++ mcs_lock/mcs_lock.h
@@ -1,20 +1,41 @@
 #pragma once
 
+#include <atomic>
+#include <thread>
 
-class MCSLock {
- public:
-  MCSLock() {
-  }
-
-  void Lock() {
-    // Your code
-  }
-
-  void Unlock() {
-    // Your code
-  }
-
- private:
-  // Your code
+struct Node {
+    std::atomic<Node*> next;
+    std::atomic<bool> acq;
 };
 
+class MCSLock {
+private:
+    std::atomic<Node*> tail{nullptr};
+    thread_local static Node node_;
+
+public:
+    MCSLock() = default;
+
+    void Lock() {
+        Node* prev = tail.exchange(&node_);
+        prev->next = &node_;
+        while (!node_.acq) {
+            std::this_thread::yield();
+        }
+    }
+
+    void Unlock() {
+        if (tail.load() != nullptr) {
+            node_.next.load()->acq = true;
+            return;
+        }
+        Node* node_pointer = &node_;
+        if (tail.compare_exchange_weak(node_pointer, nullptr)) {
+            return;
+        }
+        while (!node_.next.load()) {
+            std::this_thread::yield();
+        }
+        node_.next.load()->acq = true;
+    }
+};
