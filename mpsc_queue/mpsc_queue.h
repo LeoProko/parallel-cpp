d204c79a43757d66cf4663b97ee3d0ab14224f86
LeoProko <leoproko126@gmail.com>
2021-07-09 10:53:37 +0300

    [mpsc_queue] add solution of 'mpsc_queue' problem

--- mpsc_queue/mpsc_queue.h
+++ mpsc_queue/mpsc_queue.h
@@ -1,28 +1,51 @@
 #pragma once
 
+#include <atomic>
 #include <optional>
+
+template <typename T>
+struct Node {
+    T value;
+    Node* next;
+};
 
 
 template <typename T>
 class MPSCQueue {
- public:
-  MPSCQueue() {
-  }
+private:
+    std::atomic<Node<T>*> head_ = nullptr;
 
-  ~MPSCQueue() {
-    // Your code
-  }
+public:
+    MPSCQueue() = default;
 
-  void Push(const T& /*value*/) {
-    // Your code
-  }
+    ~MPSCQueue() {
+    }
 
-  std::optional<T> Pop() {
-    return std::nullopt;
-    // Your code
-  }
+    void Push(const T& value) {
+        Node<T>* node = new Node<T>;
+        node->value = value;
+        for (;;) {
+            node->next = head_;
+            if (head_.compare_exchange_weak(node->next, node)) {
+                return;
+            }
+        }
+    }
 
- private:
-  // Your code
+    std::optional<T> Pop() {
+        Node<T>* node_to_remove;
+        for (;;) {
+            node_to_remove = head_.load();
+            if (!head_ || (head_ && head_.compare_exchange_weak(node_to_remove, head_.load()->next))) {
+                break;
+            }
+        }
+        if (node_to_remove) {
+            T return_value = node_to_remove->value;
+            delete node_to_remove;
+            return return_value;
+        }
+        return std::nullopt;
+    }
 };
 
