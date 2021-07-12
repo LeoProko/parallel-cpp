05f88aeeae3a4b24ea35611a20cb14bc211258ac
LeoProko <leoproko126@gmail.com>
2021-07-12 14:52:21 +0300

    [mcs_lock] add solution of mcs_lock problem

--- mcs_lock/mcs_lock.h
+++ mcs_lock/mcs_lock.h
@@ -1,19 +1,43 @@
 #pragma once
 
+#include <atomic>
+#include <thread>
+#include <iostream>
+
+struct Node {
+    Node* next{nullptr};
+    bool is_locked{false};
+};
 
 class MCSLock {
- public:
-  MCSLock() {
-  }
+private:
+    std::atomic<Node*> tail{nullptr};
+    thread_local static Node node_;
 
-  void Lock() {
-    // Your code
-  }
+public:
+    MCSLock() = default;
 
-  void Unlock() {
-    // Your code
-  }
+    void Lock() {
+        Node* prev = tail.exchange(&node_);
+        if (prev) {
+            prev->next = &node_;
+            node_.is_locked = true;
+        } else {
+            return;
+        }
+        while (node_.is_locked) {
+            std::this_thread::yield();
+        }
+    }
 
- private:
-  // Your code
+    void Unlock() {
+        Node* node_pointer = &node_;
+        if (tail.compare_exchange_weak(node_pointer, nullptr)) {
+            return;
+        }
+        while (!node_.next) {
+            std::this_thread::yield();
+        }
+        node_.next->is_locked = false;
+    }
 };
